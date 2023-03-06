70e57b6d2871a8caf51a19d2d8feb83a1d655763
Huangyh1122 <55214656+Huangyh1122@users.noreply.github.com>
2023-03-06 17:14:19 -0500

    Add files via upload

---
+++ dist/mindar-face.prod.js
@@ -0,0 +1,12 @@
+import { C as o } from "./controller-e53dfd61.js";
+import { U as r } from "./ui-687dce22.js";
+const i = {
+  Controller: o,
+  UI: r
+};
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.FACE = i;
+export {
+  o as Controller,
+  r as UI
+};
---
+++ dist/mindar-image.prod.js
@@ -0,0 +1,13 @@
+import { C as o, a as r } from "./controller-8e7a4ecd.js";
+import { U as i } from "./ui-687dce22.js";
+window.MINDAR || (window.MINDAR = {});
+window.MINDAR.IMAGE = {
+  Controller: o,
+  Compiler: r,
+  UI: i
+};
+export {
+  r as Compiler,
+  o as Controller,
+  i as UI
+};
